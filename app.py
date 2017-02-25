af42537bc72d578f51c0bbf5c66d7e1a780bf9ae
Kaique da Silva <kaique.developer@gmail.com>
2017-02-25 16:36:44 -0300

    Included setup file

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 '''Axis alignment with clock app'''
+
 from sanic import Sanic
 from sanic.response import json
+
+from model import Plan
 
 app = Sanic()
 
 @app.route('/')
 async def index(request):
-    return json({'version':'0.0.1'})
+    return json({'version':'0.0.1','name':'axis_alignment_with_clocks'})
+
+@app.route('/plan')
+async def plan(request):
+    plan = Plan()
+    return json({'x':plan.axis('x').position(),'y':plan.axis('y').position(),'axis':plan.axis_count()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000)
--- app_test.py
+++ app_test.py
@@ -7,12 +7,13 @@ import requests
 import os
 
 class AppTest(unittest.TestCase):
-    #def setUp(self):
-    #    app.run(host='0.0.0.0',port=8000)
-
     def test_app_index_route(self):
         index = requests.get('http://0.0.0.0:8000')
-        self.assertEqual(index.json(),{'version':'0.0.1'})
+        self.assertEqual(index.json(),{'version':'0.0.1','name':'axis_alignment_with_clocks'})
+
+    def test_app_plan_route(self):
+        plan = requests.get('http://0.0.0.0:8000/plan')
+        self.assertEqual(plan.json(),{'axis':2,'x':0,'y':0})
 
 if __name__ == '__main__' and 'DEBUG' in os.environ and os.environ['DEBUG'] == 'true':
     unittest.main(verbosity=2)
